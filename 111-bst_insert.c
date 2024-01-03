5b8d7919757014788ff6155b004e304eeda2ab7f
Lance Desk <lncedesk@gmail.com>
2024-01-03 03:40:11 -0800

    A function that inserts a value in a Binary Search Tree

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+bst_t *create_bst_node(bst_t *parent, int value);
+
 /**
  * bst_insert - Inserts a value in a Binary Search Tree (BST).
  * @tree: A double pointer to the root node of the BST to insert the value.
@@ -18,7 +20,7 @@ bst_t *bst_insert(bst_t **tree, int value)
 
 		if (current_node == NULL)
 		{
-			new_node = binary_tree_node(current_node, value);
+			new_node = create_bst_node(current_node, value);
 			if (new_node == NULL)
 			{
 				return (NULL);
@@ -33,7 +35,7 @@ bst_t *bst_insert(bst_t **tree, int value)
 				return (bst_insert(&current_node->left, value));
 			}
 
-			new_node = binary_tree_node(current_node, value);
+			new_node = create_bst_node(current_node, value);
 			if (new_node == NULL)
 			{
 				return (NULL);
@@ -47,7 +49,7 @@ bst_t *bst_insert(bst_t **tree, int value)
 				return (bst_insert(&current_node->right, value));
 			}
 
-			new_node = binary_tree_node(current_node, value);
+			new_node = create_bst_node(current_node, value);
 			if (new_node == NULL)
 			{
 				return (NULL);
@@ -56,4 +58,24 @@ bst_t *bst_insert(bst_t **tree, int value)
 		}
 	}
 	return (NULL);
+}
+
+/**
+ * create_bst_node - Creates a new node for a Binary Search Tree (BST).
+ * @parent: Pointer to the parent node.
+ * @value: Value to store in the new node.
+ *
+ * Return: A pointer to the newly created node, or NULL on failure.
+ */
+
+bst_t *create_bst_node(bst_t *parent, int value)
+{
+	bst_t *new_node = binary_tree_node((binary_tree_t *)parent, value);
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	return (new_node);
 }
