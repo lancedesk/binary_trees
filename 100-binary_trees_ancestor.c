a19bf1fb0af988b66f8d87ba5f54b5370a73da97
Lance Desk <lncedesk@gmail.com>
2024-01-03 02:00:22 -0800

    A function that finds the lowest common ancestor of two nodes

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: A pointer to the first node.
+ * @second: A pointer to the second node.
+ *
+ * Return: A pointer to the lowest common ancestor node.
+ *         If no common ancestor was found, return NULL.
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	const binary_tree_t *temp1, *temp2;
+
+	if (first == NULL|| second == NULL)
+	{
+		return (NULL);
+	}
+
+	temp1 = first;
+	while (temp1)
+	{
+		temp2 = second;
+		while (temp2)
+		{
+			if (temp2 == temp1)
+			{
+				return ((binary_tree_t *)temp2);
+			}
+
+			temp2 = temp2->parent;
+		}
+		temp1 = temp1->parent;
+	}
+
+	return (NULL);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -21,6 +21,59 @@ struct binary_tree_s
 };
 typedef struct binary_tree_s binary_tree_t;
 
+/**
+ * struct bst_s - Binary Search Tree (BST) node structure
+ * @n: Integer value stored in the node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ * @parent: Pointer to the parent node
+ *
+ */
+
+typedef struct bst_s
+{
+	int n;
+	struct bst_s *left;
+	struct bst_s *right;
+	struct bst_s *parent;
+} bst_t;
+
+/**
+ * struct avl_s - AVL Tree node structure
+ * @n: Integer value stored in the node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ * @parent: Pointer to the parent node
+ * @balance: Balance factor of the node
+ *
+ */
+
+typedef struct avl_s
+{
+	int n;
+	struct avl_s *left;
+	struct avl_s *right;
+	struct avl_s *parent;
+	int balance;
+} avl_t;
+
+/**
+ * struct binary_heap_s - Binary Heap node structure
+ * @n: Integer value stored in the node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ * @parent: Pointer to the parent node
+ *
+ */
+
+typedef struct binary_heap_s
+{
+	int n;
+	struct binary_heap_s *left;
+	struct binary_heap_s *right;
+	struct binary_heap_s *parent;
+} heap_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -41,5 +94,28 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(
+		const binary_tree_t *first, const binary_tree_t *second
+
+		);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 #endif /* BINARY_TREES_H */
